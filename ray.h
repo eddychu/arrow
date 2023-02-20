11563b02620d00eab2fddb0bc85eeffab356355c
eddychu <hey.yadong@gmail.com>
2023-02-20 23:54:37 +0800

    first commit

---
+++ ray.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <glm/glm.hpp>
+#include <limits>
+class Ray {
+    public:
+        Ray() {}
+        Ray(const glm::vec3& a, const glm::vec3& b) { o = a; d = b; }
+        glm::vec3 origin() const { return o; }
+        glm::vec3 direction() const { return d; }
+        glm::vec3 at(float t) const { return o + t*d; }
+        
+        glm::vec3 o;
+        glm::vec3 d;
+        double t_min = std::numeric_limits<double>::epsilon();
+        double t_max = std::numeric_limits<double>::max();
+};
